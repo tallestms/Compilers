e2ff14cebe2868cce23d5aa446883a9bddaae15a
tallestms <talles@gmail.com>
2014-02-02 07:56:44 -0200

    Falta fazer FOR e SWITCH e erro no in1.gpt

--- aux.c
+++ aux.c
@@ -29,6 +29,24 @@ int verifyRelationshipComparison(int *varRelations, int currentRelationCompariso
   return 1;
 }
 
+int verifyRelationshipCondition(int *varRelations, int currentRelationComparison, int currentRelationPos)
+{
+  int i = 0;
+  int compare = varRelations[currentRelationPos-currentRelationComparison];
+  //printf("Compare: %d\n", compare);
+    if (compare == 1 || compare == 2 || compare == 4)
+  {
+    return 0;
+  }
+  for(i=currentRelationPos-currentRelationComparison;i<currentRelationPos;i++)
+  {
+    //printf("%d ", varRelations[i]);
+    if(varRelations[i] != compare || varRelations[i] == 1 || varRelations[i] == 2 || varRelations[i] == 4)
+      return 0;
+  }
+  return 1;
+}
+
 void printRelationship(int *varRelations, int currentRelationPos)
 {
   int i = 0;
